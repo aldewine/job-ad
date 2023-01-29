746b8ef1e7d58c8eb39c9848397e39009854ce22
esragucukbel <sercangucukbel@gmail.com>
2023-01-29 20:58:31 +0100

    Course work part 2

---
+++ job-board-client/src/graphql/queries.js
@@ -0,0 +1,75 @@
+import { request, gql } from 'graphql-request';
+
+const GRAPHQL_URL = "http://localhost:9000/graphql";
+  
+export async function getJob(jobId) {
+  const query = gql`
+    query jobQuery($jobId: ID!) {
+      job(jobId: $jobId) {
+        id
+        title
+        company {
+          id
+          name
+        }
+        description
+      }
+    }
+  `;
+
+  const variables = { jobId };
+  const { job } = await request(GRAPHQL_URL, query, variables);
+  return job;
+}
+
+export async function getJobs() {
+  const query = gql`
+    query jobListQuery {
+      jobs {
+        id
+        title
+        description
+        company {
+          name
+        }
+      }
+    }
+  `;
+
+  const { jobs } = await request(GRAPHQL_URL, query);
+  return jobs;
+}
+
+export async function getCompany(companyId) {
+  const query = gql`
+    query companyQuery($companyId: ID!) {
+      company(companyId: $companyId) {
+        name
+        description
+        jobs {
+          id
+          title
+          description
+        }
+      }
+    }
+  `;
+
+  const variables = { companyId };
+  const { company } = await request(GRAPHQL_URL, query, variables);
+  return company;
+}
+
+export async function createJob(input) {
+  const query = gql`
+    mutation CreateJobMutation($input: CreateJobInput!) {
+      job: createJob(input: $input) {
+        id
+      }
+    }
+  `;
+
+  const variables = { input };
+  const { job } = await request(GRAPHQL_URL, query, variables);
+  return job;
+}
--- job-board-server/resolvers.js
+++ job-board-server/resolvers.js
@@ -1,5 +1,21 @@
+import { Job, Company } from './db.js';
+
 export const resolvers = {
     Query: {
-        greeting: () => 'Hello world!',
+      jobs: () => Job.findAll(),
+      job: (_root, args) => Job.findById(args.jobId),
+      company: (_root, args) => Company.findById(args.companyId),
     },
+
+    Mutation: {
+      createJob: (_root, { input }) => Job.create(input),
+    },
+
+    Job: {
+      company: (job) => Company.findById(job.companyId), 
+    },
+
+    Company: {
+      jobs: (company) => Job.findAll((job) => job.companyId === company.id),
+    }
 };
